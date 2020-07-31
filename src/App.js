9db465a090230097a942108b1b6bbeaa25aa2ae0
BkAngel201 <33973377+BkAngel201@users.noreply.github.com>
2020-07-30 22:24:15 -0400

    MVP + stretch goals

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Route } from 'react-router-dom';
 import data from './data';
 
@@ -7,26 +7,60 @@ import Navigation from './components/Navigation';
 import Products from './components/Products';
 import ShoppingCart from './components/ShoppingCart';
 
+//ContextAPI
+import CartContext from "./contexts/CartContext"
+import ProductContext from "./contexts/ProductContext"
+
+//CustomHooks
+import { useLocalStorage } from './hooks/useLocalStorage'
+
 function App() {
 	const [products] = useState(data);
 	const [cart, setCart] = useState([]);
+	const [firstRender, setFirstRender] = useState(true)
+	const [localStorageContent, setLocalStorageContent] = useLocalStorage('CartValues','')
 
 	const addItem = item => {
-		// add the given item to the cart
+		setCart([...cart, item])
+		
 	};
+
+	const removeItem = id => {
+		setCart(
+			cart.filter(el => {
+				return el.id != id
+			})
+		)
+
+	}
+useEffect(() => {
+	if(firstRender) {
+		if(localStorageContent) {
+			setCart(localStorageContent)
+		}
+		setFirstRender(false)
+	} else {
+		setLocalStorageContent(cart)
+	}
+},[cart])
+
 
 	return (
 		<div className="App">
-			<Navigation cart={cart} />
+			<CartContext.Provider value={{cart, removeItem}}>
+				<Navigation />
+		
 
 			{/* Routes */}
-			<Route exact path="/">
-				<Products products={products} addItem={addItem} />
-			</Route>
-
-			<Route path="/cart">
-				<ShoppingCart cart={cart} />
-			</Route>
+			<ProductContext.Provider value={{products, addItem}}>
+				<Route exact path="/">
+					<Products />
+				</Route>
+			</ProductContext.Provider>
+				<Route path="/cart">
+					<ShoppingCart />
+				</Route>
+			</CartContext.Provider>
 		</div>
 	);
 }
